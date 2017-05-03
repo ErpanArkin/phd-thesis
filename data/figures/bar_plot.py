c4af1d35bccb47ec22e0d1951e1bd3dd01d8d09e
erpan <erpan14ar@gmail.com>
2017-05-03 17:31:42 +0200

    nanoribbon

--- data/figures/bar_plot.py
+++ data/figures/bar_plot.py
@@ -1,17 +1,30 @@
-# a stacked bar plot with errorbars
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
-
-a = np.loadtxt('graphene_publications.txt')
-a.sort(0)
-
-font = {'family' : 'Helvetica',
-        'size'   : 12}
-matplotlib.rc('font', **font)
+import numpy as np
+import pandas as pd
+from io import StringIO
 
 
-p1 = plt.bar(a[:,0],a[:,1])
-plt.ylabel('Publications')
+
+df = pd.read_table('graphene_publications.txt',index_col=0)
+
+fig = plt.figure() # Create matplotlib figure
+
+ax = fig.add_subplot(111) # Create matplotlib axes
+ax2 = ax.twinx() # Create another axes that shares the same x-axis as ax.
+
+width = 0.4
+
+df.publications.plot(kind='bar', color='red', ax=ax, width=width, position=1)
+df.patents.plot(kind='bar', color='blue', ax=ax2, width=width, position=0)
+
+ax.set_ylabel('publications')
+ax2.set_ylabel('patents')
 plt.xlabel('Year')
+fig.autofmt_xdate()
+lines, labels = ax.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax2.legend(lines + lines2, labels + labels2, loc=0)
+
 plt.savefig('graphene_papers.eps',bbox_inches='tight')
+
+plt.show()
