98b61c78e8bb6e9943d10c460b2a72792aae2eb5
erpan <erpan14ar@gmail.com>
2017-09-23 16:26:54 +0200

    All components are in place, the first draft is finished

--- file_search_replace.py
+++ file_search_replace.py
@@ -8,8 +8,12 @@ replace the first column text with the second column one in file bib_key_replace
 
 with open('bib_key_replace','r') as f:
     for le in f:
-        with fileinput.FileInput(['./Chapter6/chapter6.tex','./Chapter4/chapter4.tex','./Chapter5/chapter5.tex',
-        './Chapter3/chapter3.tex','./Chapter2/chapter2.tex','./Chapter1/chapter1.tex'],inplace=True) as file:
+        with fileinput.FileInput(['./Chapter1/chapter1.tex'],inplace=True) as file:
              for line in file:
-                 print(line.replace(le.split()[0], le.split()[1]), end='')
+                 if le.split()[0] is in line:
+                     input_var = input("Enter something: ")
+                     if input_var == 't':
+                      print(line.replace(le.split()[0], le.split()[1]), end='')
+                     else:
+                      continue
 f.closed
